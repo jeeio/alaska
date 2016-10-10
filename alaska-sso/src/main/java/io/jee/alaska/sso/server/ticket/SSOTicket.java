23c6a07c315fdb1e947c4e01df98ce5994b2929c
dexter <dexter@dexter-medusa>
2016-10-11 01:20:41 +0800

    Signed-off-by: dexter <dexter@dexter-medusa>

--- alaska-sso/src/main/java/io/jee/alaska/sso/server/ticket/SSOTicket.java
+++ alaska-sso/src/main/java/io/jee/alaska/sso/server/ticket/SSOTicket.java
@@ -16,7 +16,7 @@ public class SSOTicket {
 	private long time;
 
 	@Id
-	@GenericGenerator(name="uuid", strategy="org.hibernate.id.UUIDHexGenerator")
+	@GenericGenerator(name="uuid", strategy="org.hibernate.id.UUIDGenerator")
 	@GeneratedValue(generator="uuid")
 	public String getTicket() {
 		return ticket;
