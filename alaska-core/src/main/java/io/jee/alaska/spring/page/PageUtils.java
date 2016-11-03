305e1cc7dcf9ca21920f95becf4a017c9c069dc0
Maohan <Maohan@dexter-work>
2016-11-03 21:09:18 +0800

    Signed-off-by: Maohan <Maohan@dexter-work>

---
+++ alaska-core/src/main/java/io/jee/alaska/spring/page/PageUtils.java
@@ -0,0 +1,28 @@
+package io.jee.alaska.spring.page;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+public class PageUtils {
+
+	public static PageRequest toPageRequest(int page, int size) {
+		return toPageRequest(page, size, null);
+	}
+
+	public static PageRequest toPageRequest(int page, int size, Direction direction, String... properties) {
+		return toPageRequest(page, size, new Sort(direction, properties));
+	}
+	
+	public static PageRequest toPageRequest(int page, int size, Sort sort) {
+		page = (page < 1) ? 0 : page -1;
+		return new PageRequest(page, size, sort);
+	}
+
+	public static <T> PageLinked<T> toPageLinked(Page<T> page, Pageable pageable){
+		return new PageLinked<>(page, pageable);
+	}
+
+}
