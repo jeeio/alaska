019e5218cb4941f08f7f75a93cc52fa7c25c04a1
Maohan <Maohan@dexter-work>
2017-12-11 18:05:27 +0800

    加入8位UUID主键生成

---
+++ alaska-data-jpa-hibernate/src/main/java/io/jee/alaska/data/jpa/hibernate/identifier/ShortUUIDGenerator.java
@@ -0,0 +1,35 @@
+package io.jee.alaska.data.jpa.hibernate.identifier;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+/**
+ * 8位UUID生成
+ * 36的8次方
+ * @author Maohan
+ *
+ */
+public class ShortUUIDGenerator implements IdentifierGenerator {
+
+	public final static String[] chars = new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C",
+			"D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+			"Y", "Z" };
+
+	@Override
+	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+		StringBuffer shortBuffer = new StringBuffer();
+		String uuid = UUID.randomUUID().toString().replace("-", "");
+		for (int i = 0; i < 8; i++) {
+			String str = uuid.substring(i * 4, i * 4 + 4);
+			int x = Integer.parseInt(str, 16);
+			System.out.println(x);
+			shortBuffer.append(chars[x % 36]);
+		}
+		return shortBuffer.toString();
+	}
+
+}
