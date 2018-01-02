8be5ee238c462d2dea039e9c114a28a13c7f61ae
Maohan <Maohan@dexter-work>
2018-01-02 16:48:23 +0800

    把spring boot自动配置加进来算了

---
+++ alaska-boot-autoconfigure/src/main/java/io/jee/alaska/boot/autoconfigure/AlaskaConfiguration.java
@@ -0,0 +1,75 @@
+package io.jee.alaska.boot.autoconfigure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.jee.alaska.alibaba.alipay.AlipayService;
+import io.jee.alaska.alibaba.alipay.AlipayServiceImpl;
+import io.jee.alaska.alibaba.aliyun.AliyunSmsHandler;
+import io.jee.alaska.email.EmailSenderHandler;
+import io.jee.alaska.email.SimpleEmailSenderHandler;
+import io.jee.alaska.httpclient.DefaultHttpClientHelper;
+import io.jee.alaska.httpclient.HttpClientHelper;
+
+@Configuration
+@EnableConfigurationProperties(AlaskaProperties.class)
+public class AlaskaConfiguration {
+	
+	@Bean
+	@ConditionalOnClass(HttpClientHelper.class)
+	@ConditionalOnMissingBean
+	public HttpClientHelper httpClientHelper(){
+		return new DefaultHttpClientHelper();
+	}
+	
+	@ConditionalOnClass(name = "io.jee.alaska.alibaba.alipay.AlipayService")
+	static class AlipayConfiguration{
+		
+		private AlaskaProperties properties;
+		
+		public AlipayConfiguration(AlaskaProperties properties) {
+			this.properties = properties;
+		}
+		
+		@Bean
+		@ConditionalOnProperty(prefix = "alaska.alipay", name="app-id")
+		@ConditionalOnMissingBean
+		public AlipayService alipayService(){
+			return new AlipayServiceImpl(properties.getAlipay().getAppId(), properties.getAlipay().getMerchantPrivateKey(), properties.getAlipay().getAlipayPublicKey(), properties.getAlipay().isSandbox());
+		}
+		
+	}
+	
+	@ConditionalOnClass(name = "io.jee.alaska.alibaba.aliyun.AliyunSmsHandler")
+	static class AliyunSmsConfiguration{
+		
+		private AlaskaProperties properties;
+		
+		public AliyunSmsConfiguration(AlaskaProperties properties) {
+			this.properties = properties;
+		}
+		
+		@Bean
+		@ConditionalOnProperty(prefix = "alaska.aliyun", name="key-id")
+		@ConditionalOnMissingBean
+		public AliyunSmsHandler aliyunSmsHandler(){
+			return new AliyunSmsHandler(properties.getAliyun().getKeyId(), properties.getAliyun().getKeySecret());
+		}
+		
+	}
+	
+	@ConditionalOnClass(name = {"javax.mail.internet.MimeMessage","org.springframework.mail.javamail.JavaMailSender"})
+	static class EmailConfiguration{
+		
+		@Bean
+		@ConditionalOnMissingBean
+		public EmailSenderHandler emailSenderHandler(){
+			return new SimpleEmailSenderHandler();
+		}
+	}
+
+}
---
+++ alaska-boot-autoconfigure/src/main/java/io/jee/alaska/boot/autoconfigure/AlaskaProperties.java
@@ -0,0 +1,99 @@
+package io.jee.alaska.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "alaska")
+public class AlaskaProperties {
+	
+	private Alipay alipay = new Alipay();
+	private Aliyun aliyun = new Aliyun();
+	/**
+	 * 签名（邮件、短信等）
+	 */
+	private String personal;
+	
+	public Alipay getAlipay() {
+		return alipay;
+	}
+
+	public void setAlipay(Alipay alipay) {
+		this.alipay = alipay;
+	}
+	
+	public Aliyun getAliyun() {
+		return aliyun;
+	}
+
+	public void setAliyun(Aliyun aliyun) {
+		this.aliyun = aliyun;
+	}
+	
+	public String getPersonal() {
+		return personal;
+	}
+
+	public void setPersonal(String personal) {
+		this.personal = personal;
+	}
+
+	public class Alipay {
+		
+		private String appId, merchantPrivateKey, alipayPublicKey;
+		private boolean sandbox;
+
+		public String getAppId() {
+			return appId;
+		}
+
+		public void setAppId(String appId) {
+			this.appId = appId;
+		}
+
+		public String getMerchantPrivateKey() {
+			return merchantPrivateKey;
+		}
+
+		public void setMerchantPrivateKey(String merchantPrivateKey) {
+			this.merchantPrivateKey = merchantPrivateKey;
+		}
+
+		public String getAlipayPublicKey() {
+			return alipayPublicKey;
+		}
+
+		public void setAlipayPublicKey(String alipayPublicKey) {
+			this.alipayPublicKey = alipayPublicKey;
+		}
+
+		public boolean isSandbox() {
+			return sandbox;
+		}
+
+		public void setSandbox(boolean sandbox) {
+			this.sandbox = sandbox;
+		}
+		
+	}
+	
+	public class Aliyun {
+		
+		private String keyId, keySecret;
+
+		public String getKeyId() {
+			return keyId;
+		}
+
+		public void setKeyId(String keyId) {
+			this.keyId = keyId;
+		}
+
+		public String getKeySecret() {
+			return keySecret;
+		}
+
+		public void setKeySecret(String keySecret) {
+			this.keySecret = keySecret;
+		}
+	}
+	
+}
