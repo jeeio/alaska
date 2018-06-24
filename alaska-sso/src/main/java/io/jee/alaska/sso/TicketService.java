219e3c8959dffdcb4c390d8c4244d07aa24e69e4
Maohan <Maohan@dexter-shop>
2018-06-25 00:36:27 +0800

    修改一下版本

--- alaska-sso/src/main/java/io/jee/alaska/sso/TicketService.java
+++ alaska-sso/src/main/java/io/jee/alaska/sso/TicketService.java
@@ -1,6 +1,6 @@
 package io.jee.alaska.sso;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
