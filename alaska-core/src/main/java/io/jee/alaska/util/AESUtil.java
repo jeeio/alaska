c58744fea350de87c7a5ffc0219aa20d91693616
谢小旭 <sunx@jee.io>
2018-09-10 17:34:39 +0800

    Signed-off-by: 谢小旭 <sunx@jee.io>

--- alaska-core/src/main/java/io/jee/alaska/util/AESUtil.java
+++ alaska-core/src/main/java/io/jee/alaska/util/AESUtil.java
@@ -1,6 +1,14 @@
 package io.jee.alaska.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -19,17 +27,15 @@ public class AESUtil {
 	 * @param content
 	 * @param key
 	 * @return
+	 * @throws UnsupportedEncodingException 
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
 	 */
-	public static String encrypt(String content, String key) {
-		try {
-			byte[] contentBytes = content.getBytes(charset);
-			byte[] keyBytes = key.getBytes(charset);
-			byte[] encryptedBytes = encrypt(contentBytes, keyBytes);
-			return Base64.encodeBase64String(encryptedBytes);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+	public static String encrypt(String content, String key) throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
+		byte[] contentBytes = content.getBytes(charset);
+		byte[] keyBytes = key.getBytes(charset);
+		byte[] encryptedBytes = encrypt(contentBytes, keyBytes);
+		return Base64.encodeBase64String(encryptedBytes);
 	}
 
 	/**
@@ -40,17 +46,17 @@ public class AESUtil {
 	 * @param content
 	 * @param key
 	 * @return
+	 * @throws UnsupportedEncodingException 
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
+	 * @throws InvalidAlgorithmParameterException 
+	 * @throws InvalidKeyException 
 	 */
-	public static String decrypt(String content, String key) {
-		try {
-			byte[] encryptedBytes = Base64.decodeBase64(content);
-			byte[] keyBytes = key.getBytes(charset);
-			byte[] decryptedBytes = decrypt(encryptedBytes, keyBytes);
-			return new String(decryptedBytes, charset);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+	public static String decrypt(String content, String key) throws UnsupportedEncodingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		byte[] encryptedBytes = Base64.decodeBase64(content);
+		byte[] keyBytes = key.getBytes(charset);
+		byte[] decryptedBytes = decrypt(encryptedBytes, keyBytes);
+		return new String(decryptedBytes, charset);
 	}
 
 	/**
@@ -61,47 +67,64 @@ public class AESUtil {
 	 * @param contentBytes
 	 * @param keyBytes
 	 * @return
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
 	 */
-	public static byte[] encrypt(byte[] contentBytes, byte[] keyBytes) {
+	public static byte[] encrypt(byte[] contentBytes, byte[] keyBytes) throws IllegalBlockSizeException, BadPaddingException {
 		return cipherOperation(contentBytes, keyBytes, Cipher.ENCRYPT_MODE);
 	}
 
 	/**
 	 * 解密
 	 * @author xiexx, CAISAN Co.,Ltd.
-	 * @version 0.0.1, 2018-09-07 15:09:20
+	 * @version 0.0.1, 2018-09-10 15:04:11
 	 *
 	 * @param contentBytes
 	 * @param keyBytes
 	 * @return
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
+	 * @throws InvalidAlgorithmParameterException 
+	 * @throws InvalidKeyException 
 	 */
-	public static byte[] decrypt(byte[] contentBytes, byte[] keyBytes) {
+	public static byte[] decrypt(byte[] contentBytes, byte[] keyBytes) throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 		return cipherOperation(contentBytes, keyBytes, Cipher.DECRYPT_MODE);
 	}
 
 	/**
 	 * 密码操作
 	 * @author xiexx, CAISAN Co.,Ltd.
-	 * @version 0.0.1, 2018-09-07 15:10:45
+	 * @version 0.0.1, 2018-09-10 15:03:29
 	 *
 	 * @param contentBytes
 	 * @param keyBytes
 	 * @param mode
 	 * @return
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
 	 */
-	private static byte[] cipherOperation(byte[] contentBytes, byte[] keyBytes, int mode) {
+	private static byte[] cipherOperation(byte[] contentBytes, byte[] keyBytes, int mode) throws IllegalBlockSizeException, BadPaddingException {
+		SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
+		Cipher cipher = null;
 		try {
-			SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
 			byte[] initParam = IV_STRING.getBytes(charset);
 			IvParameterSpec ivParameterSpec = new IvParameterSpec(initParam);
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			cipher.init(mode, secretKey, ivParameterSpec);
-			return cipher.doFinal(contentBytes);
-		} catch (Exception e) {
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
 			e.printStackTrace();
 		}
-		return null;
-
+		return cipher.doFinal(contentBytes);
 	}
 
 }
